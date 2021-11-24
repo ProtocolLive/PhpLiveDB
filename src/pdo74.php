9662f5420981241dece99d7fbb447bcc1644d8b7
Protocol Corporation <protocollive@outlook.com>
2021-11-24 07:56:28 -0300

    Using htmlentities

--- src/pdo74.php
+++ src/pdo74.php
@@ -1,7 +1,7 @@
 <?php
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/PhpLivePDO
-//Version 2021.11.24.02
+//Version 2021.11.24.03
 
 define('PdoStr', PDO::PARAM_STR);
 define('PdoInt', PDO::PARAM_INT);
@@ -135,7 +135,7 @@ class PhpLivePdo{
               $Param[2] = PdoStr;
             endif;
           elseif($Param[2] === PdoStr and $Options['NoTag']):
-            $Param[1] = str_replace('<', '&lt;', $Param[1]);
+            $Param[1] = htmlentities($Param[1]);
           endif;
           $result->bindValue($Param[0], $Param[1], $Param[2]);
         endif;
