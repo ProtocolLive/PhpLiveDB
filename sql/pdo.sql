eaad598de02c194279fd0633167ca5c08060113c
Protocol Corporation <protocollive@outlook.com>
2022-08-02 20:50:54 -0300

    Fix log table sql

--- sql/pdo.sql
+++ sql/pdo.sql
@@ -1,12 +1,8 @@
 create table sys_logs(
   log_id int unsigned not null auto_increment primary key,
-  time datetime not null,
-  site varchar(45),
-  user_id int unsigned not null,
-  log int unsigned not null,
-  target int unsigned,
-  ip varchar(100) not null,
-  ipreverse varchar(255) not null,
-  agent varchar(255),
-  query varchar(1024) not null
+  log tinyint unsigned not null,
+  user int unsigned,
+  agent varchar(255) not null,
+  ip varchar(20) not null,
+  query text not null
 );
