9e470210e0461d463629f381cc72cb562f5bb76a
Protocol Corporation <protocollive@outlook.com>
2023-08-18 15:11:50 -0300

    Revert "New types"
    This reverts commit 6121a31c69eb25b5f098526408fa303dfbab5bc2.

--- src/Types.php
+++ src/Types.php
@@ -6,12 +6,10 @@ namespace ProtocolLive\PhpLiveDb;
 use PDO;
 
 /**
- * @version 2023.08.17.01
+ * @version 2023.08.17.00
  */
 enum Types:int{
   case Bool = PDO::PARAM_BOOL;
-  case Date = PDO::PARAM_STR;
-  case Float = PDO::PARAM_STR;
   case Int = PDO::PARAM_INT;
   case Null = PDO::PARAM_NULL;
   case Sql = 6;
