4eb64a05265273c38b413151e2c6f4ddcadcecb1
Protocol Corporation <protocollive@outlook.com>
2024-12-13 15:40:01 -0300

    Create LoadAll.php

---
+++ src/LoadAll.php
@@ -0,0 +1,26 @@
+<?php
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive/PhpLiveDb
+//Version 2024.12.13.00
+
+// Use this file if you are not working with the composer
+
+require_once(__DIR__ . '/Enums/AndOr.php');
+require_once(__DIR__ . '/Enums/Drivers.php');
+require_once(__DIR__ . '/Enums/Enginers.php');
+require_once(__DIR__ . '/Enums/FieldsGetReturn.php');
+require_once(__DIR__ . '/Enums/Formats.php');
+require_once(__DIR__ . '/Enums/Joins.php');
+require_once(__DIR__ . '/Enums/Operators.php');
+require_once(__DIR__ . '/Enums/Parenthesis.php');
+require_once(__DIR__ . '/Enums/RefTypes.php');
+require_once(__DIR__ . '/Enums/Types.php');
+require_once(__DIR__ . '/Basics.php');
+require_once(__DIR__ . '/Create.php');
+require_once(__DIR__ . '/Delete.php');
+require_once(__DIR__ . '/Field.php');
+require_once(__DIR__ . '/Insert.php');
+require_once(__DIR__ . '/InsertUpdate.php');
+require_once(__DIR__ . '/PhpLiveDb.php');
+require_once(__DIR__ . '/Select.php');
+require_once(__DIR__ . '/Update.php');
